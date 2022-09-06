2c7a68cb5f83fc73e5ac1d80a698310bafe18ed4
Moyinoluwa3 <moyogundare@gmail.com>
2022-09-06 22:12:23 +0100

    fixed some errors

--- app/routers/classes.py
+++ app/routers/classes.py
@@ -71,7 +71,7 @@ def get_result(db : Session = Depends(database.get_db)):
 @router.post("/findsubjects", response_model=List[schemas.StudentOut])
 def get_a_student(Class: str , subject: str , db : Session = Depends(database.get_db)):
     
-    student = db.query(models.Students).filter.text(models.Students.Class == Class,text(subject)).first()
+    student = db.query(models.Students).filter(models.Students.Class == Class,text(subject)).first()
     if not student:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="Not available")
     return student
