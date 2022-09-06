1371054721afc5ff22891c3d2af3da1f4e5fbabe
Moyinoluwa3 <moyogundare@gmail.com>
2022-09-06 22:44:43 +0100

    fixed some errors

--- app/routers/classes.py
+++ app/routers/classes.py
@@ -72,7 +72,7 @@ def get_result(db : Session = Depends(database.get_db)):
 @router.post("/findsubjects", response_model=List[schemas.StudentOut])
 def get_a_student(Class: str , subject: str , db : Session = Depends(database.get_db)):
     
-    student = db.query(models.Students).filter(models.Students.Class == Class,or_(models.Students.subject_1.like(subject),models.Students.subject_2.like(subject),
+    student = db.query(models.Students).filter(models.Students.Class == Class,(models.Students.subject_1.like(subject),models.Students.subject_2.like(subject),
     models.Students.subject_3.like(subject),models.Students.subject_4.like(subject),models.Students.subject_5.like(subject),models.Students.subject_6.like(subject),
     models.Students.subject_7.like(subject),models.Students.subject_8.like(subject),models.Students.subject_9.like(subject),models.Students.subject_10.like(subject))).first()
     if not student:
